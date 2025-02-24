7d57966397e4548c4659cc89a081ca4286eddbba
Charan Bandlamudi <161814268+charanbandlamudi@users.noreply.github.com>
2025-02-24 22:37:32 +0530

    Update A2.py

--- Lab Session 05/A2.py
+++ Lab Session 05/A2.py
@@ -1,3 +1,7 @@
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error, r2_score
 import numpy as np
 
